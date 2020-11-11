eaeeaf25e2257ed85aeb7ed9c479344d9c8fdf2c
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-11 14:34:42 +0100

    add js

--- pojawienie-elementu-powiekszenie-go-przez-klikniecie/app.js
+++ pojawienie-elementu-powiekszenie-go-przez-klikniecie/app.js
@@ -1,1 +1,16 @@
-const btn = document.createElement
+const btn = document.querySelector('button');
+
+const LiItems = document.querySelectorAll('li');
+let fontSize = 10;
+
+btn.addEventListener('click', function() {
+    console.log('dziala');
+    fontSize++;
+    for(let i = 0; i<LiItems.length; i++) {
+    LiItems[i].style.display = 'block';
+    LiItems[i].style.fontSize = fontSize + "px";
+
+    }
+
+})
+
