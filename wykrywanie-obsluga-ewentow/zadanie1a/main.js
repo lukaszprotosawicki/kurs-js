0a0d2bdb9802c03298d5303b2318618d47a91dde
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-11 21:01:30 +0100

    add main.js

--- wykrywanie-obsluga-ewentow/zadanie1a/main.js
+++ wykrywanie-obsluga-ewentow/zadanie1a/main.js
@@ -2,9 +2,18 @@
 
 const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', function(e) {
+const x = e.clientX;
+const y = e.clientY;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-    h1.textContent = e.clientX + ", " + e.clientY;
-    // console.log(e.clientX, e.clientY);
+    h1.textContent = x + ", " + y;
 
-    document.body.style.backgroundColor = `rgb(${e.clientX /3 }, ${e.clientY /2 }, ${e.clientX / e.clientY * 20})`;
+    const red = x / width * 100;
+    const green = y / height * 100;
+    const blue = ((x / width * 100) + (y / height * 100)) / 2;
+
+
+document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%`
+
 })
