3bc16a6e2317c5ae699dc3ba64532c101701e218
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-11 16:22:12 +0100

    add js

--- pojawienie-elementu-powiekszenie-go-przez-klikniecie/zadanie2/app.js
+++ pojawienie-elementu-powiekszenie-go-przez-klikniecie/zadanie2/app.js
@@ -2,11 +2,35 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
+const btn = document.createElement('button');
+const btnreset = document.createElement('button');
+btn.textContent = 'Dodaj 10 elementow listy';
+btnreset.textContent = 'Wyczyść';
+btn.style.fontSize = '28px';
+btnreset.style.fontSize = '28px';
+document.body.appendChild(btn);
+document.body.appendChild(btnreset);
+const ul = document.createElement('ul');
+ul.style.listStyle = "none";
+document.body.appendChild(ul);
+btn.addEventListener('click', createLiElements);
+btnreset.addEventListener('click', cleanlist);
 
 }
 
-const createEkements = () => {
+const createLiElements = () => {
+    for(let i = 0; i < 10; i++) {
+        const li = document.createElement('li');
+        li.textContent = `Element nr ${orderElement++}`;
+        li.style.fontSize = `${size++}px`
+        document.querySelector('ul').appendChild(li);
+    }
+}
 
+const cleanlist = () => {
+    document.querySelector('ul').textContent = "";
+    size = 10;
+    orderElement = 1;
 }
 
 init()
