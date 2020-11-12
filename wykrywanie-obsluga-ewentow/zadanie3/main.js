cb8c46529636575210500ad7e9e02340ed292140
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-12 21:04:12 +0100

    add main.js

--- wykrywanie-obsluga-ewentow/zadanie3/main.js
+++ wykrywanie-obsluga-ewentow/zadanie3/main.js
@@ -1,3 +1,9 @@
 const input = document.querySelector("input");
 const passwords = ['jeden', 'dwa'];
 const messages = ['super', 'działa!'];
+
+const showMessage = (e) => {
+
+}
+
+input.addEventListener('input', showMessage)
