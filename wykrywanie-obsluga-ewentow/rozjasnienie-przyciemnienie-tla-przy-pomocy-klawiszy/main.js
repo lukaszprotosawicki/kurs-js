caa734b2f43f13a6659e845716cc1114c2e3724e
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-12 17:04:52 +0100

    add js

--- wykrywanie-obsluga-ewentow/rozjasnienie-przyciemnienie-tla-przy-pomocy-klawiszy/main.js
+++ wykrywanie-obsluga-ewentow/rozjasnienie-przyciemnienie-tla-przy-pomocy-klawiszy/main.js
@@ -6,25 +6,33 @@ document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 const changeColor = (e) => {
     // console.log(e.keyCode, e.which);
+    // INSTRUKCJA IF
+    // if (e.keyCode === 38 && red < 255) {
 
-    if (e.keyCode === 38 && red < 255) {
-
-        red += 3;
-        green += 3;
-        blue += 3;
-        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    //     red += 3;
+    //     green += 3;
+    //     blue += 3;
+    //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
         
 
-    }
-    else if (e.keyCode === 40 && red > 0) {
+    // }
+    // else if (e.keyCode === 40 && red > 0) {
 
-        red -= 3;
-        green -= 3;
-        blue -= 3;
-        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    //     red -= 3;
+    //     green -= 3;
+    //     blue -= 3;
+    //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
         
+    // }
+    switch (e.keyCode) {
+        case 38:
+            document.body.style.backgroundColor = `rgb(${red <= 255 ? red++ : red}, ${green <= 255 ? green++ : green}, ${blue <= 255 ? blue++ : blue})`;
+            break;
+        case 40:
+            document.body.style.backgroundColor = `rgb(${red >= 0 ? red-- : red}, ${green >= 0 ? green-- : green}, ${blue >= 0 ? blue-- : blue})`;
+            break;
     }
-    console.log(red);
+    console.log(red)
 }
 
 window.addEventListener('keydown', changeColor)
