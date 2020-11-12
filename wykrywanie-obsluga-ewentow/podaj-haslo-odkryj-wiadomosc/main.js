e5ab1094edade483fdbffb6e419d174d089694d8
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-12 20:49:14 +0100

    add main.js

--- wykrywanie-obsluga-ewentow/podaj-haslo-odkryj-wiadomosc/main.js
+++ wykrywanie-obsluga-ewentow/podaj-haslo-odkryj-wiadomosc/main.js
@@ -1,17 +1,30 @@
 const input = document.getElementById('pass');
 const div = document.querySelector('.message')
-const password = "user";
-const message = "wyjechałam na zawsze";
+const passwords = ["user", "wiosna"];
+const messages = ["wyjechałam na zawsze", "piękna pora roku"];
 
 input.addEventListener('input', (e) => {
     // console.log(e.target.value);
-    if (password === e.target.value) {
-        div.textContent = message;
-        e.target.value = '';
-    }
-    else {
-        div.textContent = "";
-    }
+    const text = e.target.value;
+
+    passwords.forEach( (password => {
+        if(password === text);
+        div.textContent = messages;
+
+    } )
+    
+
+
+
+
+
+    // if (password === e.target.value) {
+    //     div.textContent = message;
+    //     e.target.value = '';
+    // }
+    // else {
+    //     div.textContent = "";
+    // }
 })
 
 input.addEventListener('focus', (e) => {
