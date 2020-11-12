3d5c5b0da03904ca12895d5b619648fa96a9c3fd
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-12 13:12:29 +0100

    add main.js

--- wykrywanie-obsluga-ewentow/lapanie-upuszczenie-kwadratu-zmiana-jego-koloru/main.js
+++ wykrywanie-obsluga-ewentow/lapanie-upuszczenie-kwadratu-zmiana-jego-koloru/main.js
@@ -6,17 +6,25 @@ div.style.top = `${divY}px`;
 
 let drawActive = false;
 
-div.addEventListener('mousedown', () => {
+let insertDivX;
+let insertDivY;
+
+div.addEventListener('mousedown', (e) => {
     div.style.backgroundColor = "gray"
     drawActive = true;
+    insertDivX = e.offsetX;
+    insertDivY = e.offsetY;
 })
 
 div.addEventListener('mousemove', (e) => {
     if(drawActive) {
-    divX = e.clientX;
-    divY = e.clientY;
-    div.style.left =  `${divX - 50}px`;
-    div.style.top = `${divY -50}px`;
+
+    
+
+    divX = e.clientX - insertDivX;
+    divY = e.clientY - insertDivY;
+    div.style.left =  `${divX}px`;
+    div.style.top = `${divY}px`;
     }   
 })
 
