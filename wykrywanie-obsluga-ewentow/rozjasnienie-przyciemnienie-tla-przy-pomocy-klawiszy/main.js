1a762578429bfe8fc2b7f36c615488f9df839969
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-12 16:45:35 +0100

    add js

--- wykrywanie-obsluga-ewentow/rozjasnienie-przyciemnienie-tla-przy-pomocy-klawiszy/main.js
+++ wykrywanie-obsluga-ewentow/rozjasnienie-przyciemnienie-tla-przy-pomocy-klawiszy/main.js
@@ -3,3 +3,16 @@ let green = 100;
 let blue = 100;
 
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+const changeColor = (e) => {
+    // console.log(e.keyCode, e.which);
+
+    if (e.keyCode === 38) {
+        console.log("strzalka do gory");
+    }
+    else if (e.keyCode === 40) {
+    console.log("strzalka w dol");
+    }
+}
+
+window.addEventListener('keydown', changeColor)
