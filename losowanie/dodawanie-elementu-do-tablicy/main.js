3e09f00a0e01f302ca12ee8e94a66498e67ae36b
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-13 14:22:34 +0100

    add code main.js

---
+++ losowanie/dodawanie-elementu-do-tablicy/main.js
@@ -0,0 +1,13 @@
+const names = [];
+const div = document.querySelector('div')
+
+const addName = (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input');
+    const name = input.value;
+    names.push(name);
+    // console.log(names);
+    div.textContent += name + ", ";
+}
+
+document.querySelector('button').addEventListener('click', addName)
