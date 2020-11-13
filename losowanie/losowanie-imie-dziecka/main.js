1f52b7be58cfaf247ab0907e33687e758fd7473c
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-13 12:07:33 +0100

    add function to main.js

--- losowanie/losowanie-imie-dziecka/main.js
+++ losowanie/losowanie-imie-dziecka/main.js
@@ -1,12 +1,14 @@
 const btn = document.querySelector('button');
+const div = document.querySelector('div');
 
 const names = ["Beata", "Daria", "Karolina", "Lukasz", "Joanna", "Renata", "Bartek", "Dawid"];
 
-const index = Math.floor(Math.random() * 8);
-console.log(names[index]);
+
+// console.log(names[index]);
 
 const nameGenerator = () => {
-
+    const index = Math.floor(Math.random() * 8);
+    div.textContent = `Najfajniejsze imie to ${names[index]}`
 }
 
 btn.addEventListener('click', nameGenerator)
