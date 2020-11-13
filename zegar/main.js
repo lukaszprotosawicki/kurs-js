70add4a273ba8c5073c39f5af7299962e339fd1d
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-13 18:23:34 +0100

    add time to main.js

--- zegar/main.js
+++ zegar/main.js
@@ -4,9 +4,9 @@ const time = new Date();
 
 
 
-const seconds = time.getSeconds();
-const minutes = time.getMinutes();
-const hours = time.getHours();
+const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
 
 document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`
 }
