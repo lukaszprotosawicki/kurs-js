8f92a83d5ddaa373aa80f8fa2b2355483c02e7ff
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-13 16:02:59 +0100

    add main.js

--- losowanie/losowanie-liczby/main.js
+++ losowanie/losowanie-liczby/main.js
@@ -1,6 +1,14 @@
-Math.random() * (6-4) + 4;
+Math.random() * (6 - 4) + 4;
 
 const numberRandom = (min, max) => {
     const number = Math.random() * (max - min) + min
     return number;
+}
+
+
+Math.floor(Math.random() * (6 - 4 + 1) + 4);
+
+const numberRandomInt = (min, max) => {
+const number = Math.floor(Math.random() * (max - min + 1) + min);
+return number;
 }
