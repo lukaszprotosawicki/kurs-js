f78cc343da2dcefbdcb7748acfab5b5454bf152e
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-13 14:27:23 +0100

    add code main.js

--- losowanie/dodawanie-elementu-do-tablicy/main.js
+++ losowanie/dodawanie-elementu-do-tablicy/main.js
@@ -5,9 +5,15 @@ const addName = (e) => {
     e.preventDefault();
     const input = document.querySelector('input');
     const name = input.value;
+    if(input.value.length) {
+
+  
     names.push(name);
     // console.log(names);
     div.textContent += name + ", ";
+
+    input.value = "";
+    }
 }
 
 document.querySelector('button').addEventListener('click', addName)
