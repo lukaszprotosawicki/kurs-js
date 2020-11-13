e08d7ff8e83efd7e9facac07b78703cb42ce6830
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-13 12:55:09 +0100

    add main.js

--- losowanie/generator-kodow/main.js
+++ losowanie/generator-kodow/main.js
@@ -3,12 +3,16 @@ const chars = 'ABCDEFGHIJK0123456789';
 const btn = document.querySelector('button')
 const section = document.querySelector('section')
 
-const codesNumber = 1000;
-const charsNumber = 10;
-
+const codesNumber = 10;
+const charsNumber = 15;
 const codesGenerator = () => {
-    for(let i = 0; i < codesNumber; i++) {
-        const code = "text";
+    for (let i = 0; i < codesNumber; i++) {
+        let code = "";
+        for (let i = 0; i < charsNumber; i++) {
+            const index = Math.floor(Math.random() * 21)
+
+            code += chars[index];
+        }
         const div = document.createElement('div');
         div.textContent = code;
         section.appendChild(div);
