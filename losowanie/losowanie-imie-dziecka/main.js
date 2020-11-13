b6ca94fc10e78ed8173c774c4c7af8f6768463c2
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-13 11:52:21 +0100

    add names to main.js

---
+++ losowanie/losowanie-imie-dziecka/main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('button');
+
+const name = ["Beata", "Daria", "Karolina", "Lukasz", "Joanna", "Renata", "Bartek", "Dawid"];
+const nameGenerator = () => {
+
+}
+
+btn.addEventListener('click', nameGenerator)
