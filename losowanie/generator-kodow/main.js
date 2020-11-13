a8bb80bbaab3b89c7f802c077a212a7b9079af90
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-13 12:45:38 +0100

    add main.js

---
+++ losowanie/generator-kodow/main.js
@@ -0,0 +1,7 @@
+const chars = 'ABCDEFGHIJK0123456789';
+
+const btn = document.querySelector('button')
+const section = document.querySelector('section')
+
+const codesNumber = 1000;
+const charsNumber = 10;
