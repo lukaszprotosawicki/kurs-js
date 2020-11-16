0892be24bc294e098e419e6d95a104e6bbffa8d6
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-16 16:51:46 +0100

    add main.js

---
+++ zegar/odliczanie czasu/main.js
@@ -0,0 +1,8 @@
+
+const endTime = new Date('2021-01-02 22:34:00').getTime();
+
+setInterval(() => {
+    const nowTime = new Date().getTime();
+    // const time = Math.floor((endTime - nowTime) / 1000);
+    // console.log(time);
+}, 1000)
