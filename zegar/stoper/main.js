62bb1e53c6a1dcceea4cc213b5a17754ebf19f79
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-16 19:48:23 +0100

    add main.js

--- zegar/stoper/main.js
+++ zegar/stoper/main.js
@@ -1,3 +1,22 @@
 const btnTime = document.querySelector('.main');
 const btnReset = document.querySelector('.reset');
 const panel = document.querySelector('.time div');
+
+let time = 0;
+let active = false;
+
+const timer = () => {
+    if(!active) {
+        active = !active;
+        btnTime.textContent = 'Pauza';
+    }
+    else {
+        active = !active;
+         btnTime.textContent = 'Start';
+     }
+    
+
+}
+
+btnTime.addEventListener('click', timer);
+btnReset.addEventListener('click', reset);
