6fa81d54da965f144e4259524b01509f9302b539
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-18 14:06:50 +0100

    add main.js

--- typing-efekt-pisania-na-maszynie/main.js
+++ typing-efekt-pisania-na-maszynie/main.js
@@ -1,4 +1,10 @@
 const spnText = document.querySelector('.text');
 const txt = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit delectus recusandae tenetur, consequuntur aliquam, totam enim asperiores ullam eligendi, repellat libero ad repellendus corrupti optio accusamus saepe. Mollitia repellat est tenetur vero exercitationem assumend'
 
-setInterval(addLetter, 50);
+let number = 0;
+const addLetter = () => {
+    console.log('ok')
+    number++;
+    if(number == 200) clearInterval(indexTyping);
+}
+const indexTyping = setInterval(addLetter, 50);
