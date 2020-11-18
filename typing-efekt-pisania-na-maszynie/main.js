aec103f3d05749732a7699c7fb713dc5951cb492
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-11-18 14:01:06 +0100

    add main.js

---
+++ typing-efekt-pisania-na-maszynie/main.js
@@ -0,0 +1,4 @@
+const spnText = document.querySelector('.text');
+const txt = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit delectus recusandae tenetur, consequuntur aliquam, totam enim asperiores ullam eligendi, repellat libero ad repellendus corrupti optio accusamus saepe. Mollitia repellat est tenetur vero exercitationem assumend'
+
+setInterval(addLetter, 50);
